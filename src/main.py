37750208265ab97339bb83ca737d64580f39bcbd
JozefBucany <jbucany@gmail.com>
2026-08-20 21:27:59 +0200

    copying done

--- src/main.py
+++ src/main.py
@@ -1,7 +1,33 @@
+import os
+import shutil
+
+
+def copydir(srcpath:str, destpath:str):
+    listdir = os.listdir(srcpath)
+    for item in listdir:
+        print(srcpath+"/"+item)
+        if os.path.isfile(srcpath+"/"+item):
+            shutil.copy(srcpath+"/"+item, destpath+"/"+item)
+        if os.path.isdir(srcpath+"/"+item):
+            os.mkdir(destpath+"/"+item)
+            copydir(srcpath+"/"+item, destpath+"/"+item)
+
+def copy_new():
+    if os.path.exists("./public"):
+        print("deleting Public")
+        shutil.rmtree("./public")
+        print("Creating Public")
+    os.mkdir("./public")
+    print("copying...")
+    copydir("./static", "./public")
+    print("done!")
+
+
+
 
 
 def main():
-    pass
+    copy_new()
 
 
 
